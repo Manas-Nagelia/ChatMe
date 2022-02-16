e19b6140a65196e0eaf5b9997deee2c290a86902
Manas Nagelia <84251015+undevable@users.noreply.github.com>
2022-02-15 19:28:53 -0500

    Revert "Added tailwind to repo"
    This reverts commit d38cee7e704f9519ad03c404650e920fa207c3e2.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // ! This sass options is not working
-  // sassOptions: {
-  //   includePaths: [path.join(__dirname, "styles")],
-  // },
 };
 
 module.exports = nextConfig;
