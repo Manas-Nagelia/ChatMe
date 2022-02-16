ee81d5597f78add2b4fb320bada1cd3a93bbc43c
Manas Nagelia <84251015+undevable@users.noreply.github.com>
2022-02-15 19:29:36 -0500

    Revert "Added Sass to repo"
    This reverts commit 59d15350f2f03c63dd8dc8f46fa7ede403133fe7.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+<<<<<<< HEAD
 };
+=======
+}
+>>>>>>> parent of 59d1535 (Added Sass to repo)
 
-module.exports = nextConfig;
+module.exports = nextConfig
