d77a26969eb59cf45d8bbefccb0d68d2f281ce7c
Manas Nagelia <84251015+undevable@users.noreply.github.com>
2022-02-15 19:30:11 -0500

    Removed config from Next config file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> parent of 59d1535 (Added Sass to repo)
 
 module.exports = nextConfig
