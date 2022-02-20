b7227579d15697275291bbee4f68d248a0e6759e
Manas Nagelia <84251015+undevable@users.noreply.github.com>
2022-02-20 16:33:25 -0500

    Added alternative urls

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/home",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/dashboard",
+        destination: "/chats",
+        permanent: true,
+      },
+      {
+        source: "/chat",
+        destination: "/chats",
+        permanent: true,
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig
