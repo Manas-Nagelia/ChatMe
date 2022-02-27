065a49c4399cd9dafb455048eb92a3959fadf134
Manas Nagelia <84251015+undevable@users.noreply.github.com>
2022-02-27 18:44:15 -0500

    CHATME-60 Rendered out the search UI

--- components/chats.tsx
+++ components/chats.tsx
@@ -3,10 +3,13 @@ import { useState, useEffect } from "react";
 import { useRealtime } from "react-supabase";
 import Loading from "./Loading";
 import { supabase } from "../utils/db/supabaseClient";
+import Link from "next/link";
 
 const Chats: NextPage = () => {
   const [loading, setLoading] = useState(true);
   const [message, setMessage] = useState("");
+  const [user, setUser] = useState("");
+  const [autocomplete, setAutocomplete] = useState([]);
 
   const [{ data, error }] = useRealtime("messages", {
     select: {
@@ -18,17 +21,29 @@ const Chats: NextPage = () => {
 
   if (error) console.log(error);
 
-  const fetchData = async () => {
-    const { data, error } = await supabase.rpc("search_last_name", {
-      search_query: "Nage",
-    });
-
-    console.log(data);
-  };
-
   useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.rpc("search_first_name", {
+        search_query: user,
+      });
+
+      if (data.length === 0) {
+        const { data, error } = await supabase.rpc("search_last_name", {
+          search_query: user,
+        });
+
+        if (!error) setAutocomplete(data);
+        else console.log(error);
+      } else {
+        if (!error) setAutocomplete(data);
+        else console.log(error);
+
+        setAutocomplete(data);
+      }
+    };
+
     fetchData();
-  }, []);
+  }, [user]);
 
   const sendMessage = async () => {
     const userId = supabase.auth.user().id;
@@ -48,6 +63,31 @@ const Chats: NextPage = () => {
     <div>
       <h1>Your chats</h1>
       {loading && <Loading />}
+      <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="user">User: </label>
+        <input
+          type="text"
+          name="user"
+          placeholder="Who do you want to chat with?"
+          value={user}
+          onChange={(e) => {
+            setUser(e.target.value);
+          }}
+        />
+      </form>
+      {user != "" && autocomplete.length === 0 && <p>No user found with that name</p>}
+      {autocomplete && autocomplete.length > 0 && (
+        <ul>
+          {autocomplete.map((item) => (
+            <li key={item.id}>
+              <Link href={`chat/${item.id}`}>
+                <a>{item.first_name + " " + item.last_name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      <hr />
       {data && data.map((message) => <p key={message.id}>{message.message}</p>)}
       <form
         onSubmit={(e) => {
