ab1b3a574061c25b90e531d086bda225e8b6d170
Manas Nagelia <84251015+undevable@users.noreply.github.com>
2022-03-08 17:20:23 -0500

    Added transitions to connections UI

--- modules/chat/components/ConnectionUI.tsx
+++ modules/chat/components/ConnectionUI.tsx
@@ -3,8 +3,15 @@ import { Paper, Navbar, Button, Space, Container } from "@mantine/core";
 import { NameObject } from "../interfaces/NameObject";
 import { ConnectionProps } from "../interfaces/ConnectionProps";
 import { Text } from "@mantine/core";
+import { useRouter } from "next/router";
+import { useMantineTheme } from "@mantine/core";
 
 const ConnectionUI: NextPage<ConnectionProps> = (props) => {
+  const router = useRouter();
+  const { id } = router.query;
+  const theme = useMantineTheme();
+
+  console.log(id);
   return (
     <>
       {props.names &&
@@ -12,14 +19,27 @@ const ConnectionUI: NextPage<ConnectionProps> = (props) => {
         props.names.map((name: NameObject) => (
           <Navbar.Section key={name.id} mt="md">
             <Paper
-              sx={(theme) => ({ backgroundColor: theme.colors.brand[6], cursor: "pointer", ":hover": { backgroundColor: theme.colors.brand[7] } })}
+              sx={(theme) => ({
+                backgroundColor:
+                  id === name.id ? theme.colors.brand[6] : "white",
+                border: `0.5px solid ${theme.colors.gray[9]}`,
+                cursor: "pointer",
+                transition: "all 200ms ease-in-out",
+                ":hover": { border: `0.5px solid ${theme.colors.brand[6]}`, transition: "all 200ms ease-in-out" },
+              })}
               padding="xs"
               onClick={(e: any) => console.log("y")}
             >
-              <Text color="white" weight="bold">
+              <Text
+                color={id === name.id ? "#e6e6e6" : theme.colors.dark[9]}
+                weight="bold"
+              >
                 {name.name}
               </Text>
-              <Text size="xs" color="#e6e6e6">{`(${name.email})`}</Text>
+              <Text
+                size="xs"
+                color={id === name.id ? "#e6e6e6" : theme.colors.gray[7]}
+              >{`(${name.email})`}</Text>
             </Paper>
           </Navbar.Section>
         ))}
