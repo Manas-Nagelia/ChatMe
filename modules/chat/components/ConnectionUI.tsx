b171cd45e46ff5c60dbd2e7b169167628e9bab3b
Manas Nagelia <84251015+undevable@users.noreply.github.com>
2022-03-08 17:20:04 -0500

    Updated connections UI

--- modules/chat/components/ConnectionUI.tsx
+++ modules/chat/components/ConnectionUI.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import { Paper, Navbar } from "@mantine/core";
+import { Paper, Navbar, Button, Space, Container } from "@mantine/core";
 import { NameObject } from "../interfaces/NameObject";
 import { ConnectionProps } from "../interfaces/ConnectionProps";
 import { Text } from "@mantine/core";
@@ -11,8 +11,14 @@ const ConnectionUI: NextPage<ConnectionProps> = (props) => {
         props.names.length > 0 &&
         props.names.map((name: NameObject) => (
           <Navbar.Section key={name.id} mt="md">
-            <Paper sx={(theme) => ({ backgroundColor: theme.colors.brand[6] })} padding="xs">
-              <Text color="white" weight="bold">{name.name}</Text>
+            <Paper
+              sx={(theme) => ({ backgroundColor: theme.colors.brand[6], cursor: "pointer", ":hover": { backgroundColor: theme.colors.brand[7] } })}
+              padding="xs"
+              onClick={(e: any) => console.log("y")}
+            >
+              <Text color="white" weight="bold">
+                {name.name}
+              </Text>
               <Text size="xs" color="#e6e6e6">{`(${name.email})`}</Text>
             </Paper>
           </Navbar.Section>
